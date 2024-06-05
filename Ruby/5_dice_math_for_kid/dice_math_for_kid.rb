ee49daa70887b0ceba36740968cf7ca91865add8
ytiruces <pavelkulkov@gmail.com>
2024-06-05 17:26:47 +0300

    update

--- Ruby/5_dice_math_for_kid/dice_math_for_kid.rb
+++ Ruby/5_dice_math_for_kid/dice_math_for_kid.rb
@@ -1,17 +1,30 @@
 # введите количество кубиков N
 puts "Введите количество кубиков:"
 cubes = gets.to_i
+dots = 1..6
 # выводим вопрос "Бросаем N кубиков?"
 puts "Бросаем #{cubes} кубика"
 # выводим случайно число от 1 до 6 + случайное число от 1 до 6 N раз
-dots = 1..6
+
+dice = Array.new(cubes)
+
 loop do
-  puts "\nБросаем?"
+  print "Бросаем?"
   gets
-  cubes.times {print "  #{rand(dots)}"}
-  puts ""
+  for cube in 0..cubes-1 do
+    dice[cube] = rand(dots)
+    print "  #{dice[cube]}"
+  end
+  puts "\nСумма = ?"
+  result = gets.to_i
+  if dice.sum == result
+    puts "ПРАВИЛЬНО, МОЛОДЕЦ! :)"
+  else
+    puts 'УПС :('
+  end
 end
-# выводим вопрос "Сколько выпало?" 
+# выводим вопрос "Сколько выпало?"
+
 # проверяем ответ с суммой
 # отвечаем "верно" или "не верно" 
 # повторяем со второй строки
